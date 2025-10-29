6a6b630f29e908729a5d3696edce67c57122e376
Haiqal <haiqalaly@gmail.com>
2025-10-29 15:51:37 +0700

    adding player animation based on mouse direction

--- script/bullet.gd
+++ script/bullet.gd
@@ -3,8 +3,8 @@ extends Area2D
 var direction: Vector2
 
 func setup(pos: Vector2, dir: Vector2):
-	position = pos + dir * 18
+	position = pos + dir * 17
 	direction = dir
 	
 func _physics_process(delta: float) -> void:
-	position += direction * 40 * delta
+	position += direction * 80 * delta
--- script/player.gd
+++ script/player.gd
@@ -2,10 +2,20 @@ extends CharacterBody2D
 
 var direction_x: float
 var speed := 50
-@export var jump_strenght := 10
-@export var gravity := 10
+@export var jump_strenght := 350
+@export var gravity := 600
 var can_shoot := true
 const SHOOT_COOLDOWN := 0.5
+const gun_direction = {
+	Vector2i(1,0):   0,
+	Vector2i(1,1):   1,
+	Vector2i(0,1):   2,
+	Vector2i(-1,1):  3,
+	Vector2i(-1,0):  4,
+	Vector2i(-1,-1): 5,
+	Vector2i(0,-1):  6,
+	Vector2i(1,-1):  7,
+}
 
 signal shoot(pos: Vector2, dir: Vector2)
 
@@ -25,3 +35,14 @@ func _physics_process(delta: float) -> void:
 	velocity.x = direction_x * speed
 	apply_gravity(delta)
 	move_and_slide()
+	animation()
+	
+func animation():
+	$Legs.flip_h = direction_x  < 0
+	if is_on_floor():
+		$AnimationPlayer.current_animation = 'run' if direction_x else 'idle'
+	else:
+		$AnimationPlayer.current_animation = 'jump'
+	var raw_direction = get_global_mouse_position() - global_position
+	var adjusted_dir = Vector2i(round(raw_direction.normalized().x), round(raw_direction.normalized().y))
+	$Torso.frame = gun_direction[adjusted_dir]
