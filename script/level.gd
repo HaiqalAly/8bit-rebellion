913fa9e642d7c5d0452a89e37d2d7b9e43646a49
Haiqal <haiqalaly@gmail.com>
2025-10-31 20:29:36 +0700

    Level design

--- script/level.gd
+++ script/level.gd
@@ -4,6 +4,6 @@ var bullet_scene = preload("res://scenes/Entities/bullet.tscn")
 
 func _on_player_shoot(pos: Vector2, dir: Vector2) -> void:
 	var bullet = bullet_scene.instantiate() as Area2D
-	$Bullet.add_child(bullet)
+	$Bullets.add_child(bullet)
 	bullet.setup(pos, dir)
 	
